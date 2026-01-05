412c1c482336b03045b6f6005316db1ef27f3acd
Hussein Elmasri <helmasri88@gmail.com>
2026-01-05 20:31:24 +0100

    .

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -4,8 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey:
-    process.env.REACT_APP_API_KEY || 'AIzaSyBv1DdBew8mSF2nsrS49iApNinHK3wtFVs',
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
