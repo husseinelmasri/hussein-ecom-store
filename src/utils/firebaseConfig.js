2cf10f207d8a5cdd805f6b9099c41bdf2cf1cf5b
Hussein Elmasri <helmasri88@gmail.com>
2026-01-05 23:01:49 +0100

    adjusted

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -4,12 +4,22 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey:
+    process.env.REACT_APP_API_KEY || 'AIzaSyBv1DdBew8mSF2nsrS49iApNinHK3wtFVs',
+  authDomain:
+    process.env.REACT_APP_AUTH_DOMAIN || 'hussein-ecom-app.firebaseapp.com',
+
+  projectId: process.env.REACT_APP_PROJECT_ID || 'hussein-ecom-app',
+  storageBucket:
+    process.env.REACT_APP_STORAGE_BUCKET ||
+    'hussein-ecom-app.firebasestorage.app',
+
+  messagingSenderId:
+    process.env.REACT_APP_MESSAGING_SENDER_ID ||
+    '1:254395264756:web:c9df0e4b0099cc7037f8a1',
+
+  appId:
+    process.env.REACT_APP_ID || '1:254395264756:web:c9df0e4b0099cc7037f8a1',
 };
 
 const app = initializeApp(firebaseConfig);
